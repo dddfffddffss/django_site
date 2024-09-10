f4dcc806b7e68775281879b667751d76886ebb58
dddfffddffss <dlhwadlhwa@gmail.com>
2024-09-10 21:58:25 +0900

    las

--- pybo/models.py
+++ pybo/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Question(models.Model):
-    subject = models.CharField(max_length=200)
-    content = models.TextField()
-    create_date = models.DateTimeField()
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	subject = models.CharField(max_length=200)
+	content = models.TextField()
+	create_date = models.DateTimeField()
+	modify_date = models.DateTimeField(null=True, blank=True)
 
-    def __str__(self):
-        return self.subject
+	def __str__(self):
+		return self.subject
 
 class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    content = models.TextField()
-    create_date = models.DateTimeField()
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	question = models.ForeignKey(Question, on_delete=models.CASCADE, null=False, blank=False)
+	content = models.TextField(null=False, blank=False)
+	create_date = models.DateTimeField()
+	modify_date = models.DateTimeField(null=True, blank=True)
